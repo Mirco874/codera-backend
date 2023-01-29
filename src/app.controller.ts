63a5b24b0d551a377649112bc41d7fc99e2b94cc
Mirco Estefano Pilco Peralta <mircocuof@gmail.com>
2023-01-29 15:37:16 -0400

    added restrictions to create and account

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,28 +1,56 @@
-import { Controller, Get, Post,Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  BadRequestException,
+} from '@nestjs/common';
 import { ValidationPipe } from '@nestjs/common/pipes';
-import { AppService} from './app.service';
+import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
 import { LoginUserDTO } from './auth/dto/loginUser.dto';
 import { CreateUserDTO } from './User/dto/CreateUser.dto';
 import { User } from './User/entities/User.entity';
+import { UserService } from './User/service/User.service';
 
-@Controller("/api/v1/")
+@Controller('/api/v1/')
 export class AppController {
-  constructor(private readonly appService: AppService, private authService: AuthService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @Post("auth/login")
-  async login(@Body() loginUserDTO:LoginUserDTO){
-    return this.authService.login(loginUserDTO)
+  @Post('auth/login')
+  async login(@Body() loginUserDTO: LoginUserDTO) {
+    return this.authService.login(loginUserDTO);
   }
 
-  @Post("auth/register")
-  async register(@Body(ValidationPipe) createUserDTO:CreateUserDTO):Promise<User>{
+  @Post('auth/register')
+  async register(
+    @Body(ValidationPipe) createUserDTO: CreateUserDTO,
+  ): Promise<User> {
+    const findUser = await this.userService.findByEmail(createUserDTO.email);
+
+    if (findUser) {
+      throw new BadRequestException(
+        `There is a user with the email: ${createUserDTO.email} registered.`,
+      );
+    }
+
+    if (createUserDTO.password.length < 8) {
+      throw new BadRequestException(
+        `The password must be at least 8 characters`,
+      );
+    }
+
     return this.authService.registerUser(createUserDTO);
   }
+
 
 }
