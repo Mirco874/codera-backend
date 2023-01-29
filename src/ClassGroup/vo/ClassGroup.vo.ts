c985bf41ad9dd94af62f4f2739880848afa19d12
Mirco Estefano Pilco Peralta <mircocuof@gmail.com>
2023-01-29 15:10:32 -0400

    added type date for creation date of class

--- src/ClassGroup/vo/ClassGroup.vo.ts
+++ src/ClassGroup/vo/ClassGroup.vo.ts
@@ -1,28 +1,9 @@
-import { TeacherVO } from "src/User/vo/Teacher.vo";
+import { TeacherVO } from 'src/User/vo/Teacher.vo';
 
 export class ClassGroupVO {
   id: string;
   className: string;
   classDescription: string;
-  instructor:TeacherVO;
-
-  setId(id): void {
-    this.id = id;
-  }
-
-  setClassName(className): void {
-    this.className = className;
-  }
-
-  setInstructor(instructor):void{
-    this.instructor=instructor;
-  }
-
-  setClassDescription(classDescription): void {
-    this.classDescription = classDescription;
-  }
-
-  getInstructor():TeacherVO{
-    return this.instructor;
-  }
+  instructor: TeacherVO;
+  creationDate: Date;
 }
